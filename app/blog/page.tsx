667bf2fdc411802891e362c0a86f3e19edf44f38
Basilin Joe <basilinjoe@gmail.com>
2024-01-30 10:32:38 +0530

    Add sitemap

---
+++ app/blog/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link"
+
+import { siteConfig } from "@/config/site"
+import { buttonVariants } from "@/components/ui/button"
+import { Badge } from "@/components/ui/badge"
+
+export default function About() {
+  return (
+    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+      Under construction
+    </section>
+  )
+}
