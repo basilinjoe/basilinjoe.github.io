8058b8f171751eb5f70cc6f20e80128b336b6500
basilinjoe <basilinjoe@gmail.com>
2025-08-05 18:04:41 +0530

    refactor: Remove basePath and assetPrefix configuration for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@ const ghPages = process.env.DEPLOY_TARGET === 'gh-pages';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    basePath: ghPages? "/basilinjoe.github.io" : "",
-    assetPrefix: ghPages ? "/basilinjoe.github.io/": "",
+    basePath: "",
+    assetPrefix: "",
     images: {
         unoptimized: true,
     },
